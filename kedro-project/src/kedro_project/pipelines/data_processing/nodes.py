eeb50d1dc69862533ddf663d628fcb1755e6d462
Diego <diegosanchez1234567@gmail.com>
2024-09-12 22:42:39 -0300

    agregar notebook informe tecnico

--- kedro-project/src/kedro_project/pipelines/data_processing/nodes.py
+++ kedro-project/src/kedro_project/pipelines/data_processing/nodes.py
@@ -12,3 +12,19 @@ def filter_numeric_columns(df: pd.DataFrame) -> pd.DataFrame:
     numeric_df = df.select_dtypes(include=[np.number])
 
     return numeric_df
+
+
+def eliminar_columnas(data: pd.DataFrame, columnas: list) -> pd.DataFrame:
+    """
+    Elimina las columnas especificadas de un DataFrame.
+
+    Args:
+        data (pd.DataFrame): El DataFrame de entrada.
+        columnas (list): Lista de nombres de columnas a eliminar.
+
+    Returns:
+        pd.DataFrame: El DataFrame resultante con las columnas eliminadas.
+    """
+    # Eliminar las columnas especificadas
+    data = data.drop(columns=columnas, errors='ignore')
+    return data
--- kedro-project/src/kedro_project/pipelines/data_processing/pipeline.py
+++ kedro-project/src/kedro_project/pipelines/data_processing/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.19.8
 
 from kedro.pipeline import Pipeline, pipeline, node
 
-from .nodes import filter_numeric_columns
+from .nodes import filter_numeric_columns, eliminar_columnas
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -15,5 +15,12 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs="games",
             outputs="numeric_games",
             name="filter_numeric_columns_node",
+        ),
+        node(
+            func=eliminar_columnas,
+            inputs=["games", "params:columnas_a_eliminar"],
+            outputs="games_new",
+            name="eliminar_columnas_node",
         )
     ])
+
